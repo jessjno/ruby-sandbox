680719967c1d58c0c01cddb8c122dfe82ee6c247
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-04-27 14:02:36 +0000

    updated non shall pass

--- none_shall_pass.rb
+++ none_shall_pass.rb
@@ -2,56 +2,118 @@ EMAIL = "jess@example.com"
 PASSWORD = "gocode"
 PASSWORD_VAULT = {aws: {username: "jess", password: "jno"}}
 
-puts "Welcome to None Shall Pass - A Password Manager"
-puts "Please sign in to continue"
-print "Enter email: "
+def welcome_message
+  puts "Welcome to None Shall Pass - A Password Manager"
+end
 
-user_email = gets.chomp
-p user_email
+def prompt_user_for_email
+  puts "Please sign in to continue"
+  print "Enter email: "
+  gets.chomp
+end
 
-if user_email == EMAIL
+def verify_user_email(user_email)
+  if user_email != EMAIL
+    puts "Invalid Email"
+    exit
+  end 
+end
+
+def prompt_user_for_password
   print "Enter password: "
-  user_password = gets.chomp
-  else
-  puts "Invalid Email"
-  end
+  gets.chomp
+end
 
-unless user_password != PASSWORD
+def verify_user_password(user_password)
+  if user_password != PASSWORD
+    puts "Invalid Password"
+    exit
+  end
+end
+
+def greet_user(user_email)
   puts "Hello, #{user_email}"
   puts "What would you like to do?"
+end
+
+def menu_options
   puts "1. Add new service credentials"
   puts "2. Retrieve an existing services credentials"
   puts "3. Exit"
-  user_selection = gets.chomp
-  else 
-    puts "Invalid Password"
 end
 
-case user_selection
-when "1"
-  puts "Enter the name of the service: "
-  new_service = gets.chomp
-  PASSWORD_VAULT[new_service] 
-  
-  print "Enter the username for this new service: "
-  new_service_username = gets.chomp
-  PASSWORD_VAULT[new_service]["username"] = new_service_username
 
-  print "Enter the password for this new service: "
-  new_service_password = gets.chomp
-  PASSWORD_VAULT[new_service]["password"] = new_service_password
+def get_user_menu_selection
+  gets.chomp
+end
 
+def handle_user_selection(user_selection)
+  case user_selection
+  when "1"
+    new_service = set_new_service_name
+    set_username_for(new_service)
+    set_password_for(new_service)
   when "2"
-  print "Please enter the name of the service you wish to access credentials for: "
-  requested_service_name = gets.chomp
-  PASSWORD_VAULT[requested_service_name.to_sym]
-  credentials = PASSWORD_VAULT[requested_service_name.to_sym]
-  puts "Here are the credentials for #{requested_service_name}:"
-  credentials.each do |key, val|
-    puts "#{key}: #{val}"
+    requested_service_name = retrieve_service_name
+    credentials = retrieve_service_credentials_for(requested_service_name)
+    display_service_credentials(requested_service_name, credentials)
+  else
+    exit_program
   end
-else
-  puts "EXITING THE PROGRAM"
-  exit
-end 
+end
 
+  def set_new_service_name
+    print "Enter the name of the service: "
+    new_service = gets.chomp
+    PASSWORD_VAULT[new_service.to_sym] = {}
+    new_service
+  end
+
+  def set_username_for(service)
+    print "Enter the username for this new service: "
+    new_service_username = gets.chomp
+    PASSWORD_VAULT[service.to_sym][:username] = new_service_username
+  end
+
+  def set_password_for(service)
+    print "Enter the  password name for this new service: "
+    new_service_password = gets.chomp
+    PASSWORD_VAULT[service.to_sym][:password] = new_service_password
+  end
+
+  def retrieve_service_name
+    print "Please enter the name of the service you wish to access credentials for: "
+    gets.chomp
+  end
+
+  def retrieve_service_credentials_for(service_name)
+    PASSWORD_VAULT[service_name.to_sym]
+  end
+
+  def display_service_credentials(service_name, credentials)
+    puts "Here are the credentials for #{service_name}:"
+    credentials.each do |key, val|
+      puts "#{key}: #{val}"
+    end
+  end
+
+  def exit_program
+    puts "Exiting the PROGRAM"
+    exit
+  end
+
+
+
+
+welcome_message
+user_email = prompt_user_for_email
+verify_user_email(user_email)
+user_password = prompt_user_for_password
+verify_user_password(user_password)
+greet_user(user_email)
+
+loop do
+  menu_options
+  user_selection = get_user_menu_selection
+  handle_user_selection(user_selection)
+end
