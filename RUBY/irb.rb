bd72c566409902a175aacfb1fbb58996d00e0a74
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-06-01 18:23:42 +0000

    created a calculator

--- RUBY/irb.rb
+++ RUBY/irb.rb
@@ -1,3 +1,5 @@
 puts 5 != 2
  
 #Just using this file for random practice
+
+
---
+++ calculator.rb
@@ -0,0 +1,20 @@
+class Calculator
+  def plus(number, other)
+    number.to_f + other
+  end
+  def minus(number, other)
+    number.to_f - other
+  end
+  def multiply(number, other)
+    number.to_f * other
+  end
+  def divide(number, other)
+    number.to_f / other
+  end
+end
+
+calculator = Calculator.new
+puts calculator.plus(2, 3)
+puts calculator.minus(2, 3)
+puts calculator.multiply(2,3)
+puts calculator.divide(2, 3)
